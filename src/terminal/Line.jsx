010f9dad4ffaf1162ca3126761a6ee9a11226f7b
Seth Lowie <slowie@powerchordsystem.com>
2018-01-29 21:43:02 -0500

    enter to exec active line

--- src/terminal/Line.jsx
+++ src/terminal/Line.jsx
@@ -2,29 +2,42 @@
 import React, { Component } from 'react';
 import './line.scss';
 
+const highlight = str => ({
+  __html: `${str.replace(/\b(function|git)/gmi, '<span style="color: #00ffec;">$1</span>')}<div class="cursor"></div>`
+});
+
 class Line extends Component {
   state = {
     syntax: [
-      <div className="active-line"><span>waffles are great</span><div className="cursor"></div></div>
+      <div className="active-line"><div className="cursor"></div></div>
     ],
-    raw: 'waffles are great',
+    raw: '',
   }
   componentDidMount = () => {
     document.addEventListener('keydown', (event) => {
       const key = event.key;
-      console.log(key === ' ')
-      const newRaw = `${this.state.raw}${this.parseLine(event.key)}`;
+      const newRaw = this.parseLine(this.state.raw, event.key);
       this.state.raw = newRaw;
-      this.state.syntax[0] = <div className="active-line"><span>{newRaw}</span><div className="cursor"></div></div>
+      this.state.syntax[0] = <div className="active-line" dangerouslySetInnerHTML={highlight(newRaw)}></div>
       this.setState(this.state);
 
     })
   }
-  parseLine = (key: String) => {
-    if (key === 'Shift') {
+  execLine = (str: String) => {
+    this.props.exec(str);
+  }
+  parseLine = (str: String, key: String) => {
+    if (key === 'Enter') {
+      this.execLine(str);
       return '';
     }
-    return key === ' ' ? '\xa0' : key;
+    if (key === 'Shift') {
+      return str;
+    }
+    if (key === 'Backspace') {
+      return str.substring(0, str.length - 1);
+    }
+    return `${str}${key === ' ' ? '\xa0' : key}`;
   }
   render() {
     const { syntax, raw } = this.state;
