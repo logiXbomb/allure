130ca32c9ff67944110fc7e3b9eec61606bc1412
Seth Lowie <slowie@powerchordsystem.com>
2018-01-30 19:12:59 -0500

    update terminal to output shell response

--- src/terminal/Terminal.jsx
+++ src/terminal/Terminal.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react';
+import { ipcRenderer } from 'electron';
 import Line from './Line';
 import './terminal.scss';
 
@@ -7,14 +8,20 @@ class Terminal extends Component {
   state = {
     output: []
   }
+  componentDidUpdate = () => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }
   exec = (str: String) => {
-    this.state.output.push(str);
+    // this.state.output.push(str);
+    const response = ipcRenderer.sendSync('exec', str);
+    console.log('resp', response);
+    this.state.output = this.state.output.concat(response.split(/\r?\n/))
     this.setState(this.state);
   }
   render() {
     return <div className="terminal">
       <div className="output">
-        {this.state.output.map(o => <span>{o}</span>)}
+        {this.state.output.map(o => <div>{o.replace(/ /gmi, '\xa0')}</div>)}
       </div>
       <Line exec={this.exec} />
     </div>
