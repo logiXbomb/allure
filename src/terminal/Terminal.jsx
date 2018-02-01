2ea461cb36b0ad9a2f0b8b20b56375231a8670b5
Seth Lowie <slowie@powerchordsystem.com>
2018-01-31 21:59:07 -0500

    fork path for known commands

--- src/terminal/Terminal.jsx
+++ src/terminal/Terminal.jsx
@@ -1,22 +1,37 @@
 // @flow
 import React, { Component } from 'react';
 import { ipcRenderer } from 'electron';
+import commands from '../commands';
 import Line from './Line';
 import './terminal.scss';
 
 class Terminal extends Component {
   state = {
-    output: []
+    output: [],
+    dir: '~/'
+  }
+  componentDidMount = () => {
+    ipcRenderer.on('output', (x ,data) => {
+      console.log('data', x ,data)
+      this.state.output = this.state.output.concat(data.toString().split(/\r?\n/))
+      this.setState(this.state);
+    });
   }
   componentDidUpdate = () => {
     window.scrollTo(0, document.body.scrollHeight);
   }
   exec = (str: String) => {
     // this.state.output.push(str);
-    const response = ipcRenderer.sendSync('exec', str);
-    console.log('resp', response);
-    this.state.output = this.state.output.concat(response.split(/\r?\n/))
-    this.setState(this.state);
+    const command = str.split(/\xa0/);
+    if (commands[command[0]]) {
+      console.log('perform known command');
+      ipcRenderer.send('cd', command[1]);
+    } else {
+      ipcRenderer.send('exec', {
+        command: str,
+        dir: this.state.dir,
+      });
+    }
   }
   render() {
     return <div className="terminal">
