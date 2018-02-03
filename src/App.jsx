ee251752abb48894482f1047f8dd7d83108fe0b8
Seth Lowie <slowie@powerchordsystem.com>
2018-02-02 23:40:16 -0500

    add new terminals

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
+import short from 'short-uuid';
 import Terminal from './terminal';
 import './app.scss';
 
-const App = () => <div className="app">
-  <Terminal />
-</div>;
+class App extends Component {
+  state = {
+    terminals: [],
+  }
+  componentDidMount = () => {
+    this.addTerminal();
+  }
+  addTerminal = (obj) => {
+    if (obj) {
+      console.log('---->', obj.offsetWidth);
+      window.resizeBy(obj.offsetWidth, 0);
+    }
+    this.state.terminals.push({
+      id: short.uuid(),
+      dir: '',
+    });
+    this.setState(this.state);
+  }
+  removeTerminal = id => {
+    this.state.terminals = this.state.terminals.filter(t => t.id !== id);
+    this.setState(this.state);
+  }
+  render() {
+    return <div className="app">
+      {this.state.terminals.map(t => 
+        <Terminal
+          key={t.id}
+          addTerminal={this.addTerminal}
+          removeTerminal={this.removeTerminal}
+          id={t.id}
+          dir={t.dir}
+        />
+      )}
+    </div>
+  }
+}
 
 export default App;
